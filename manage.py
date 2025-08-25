a3447e04de4c81f169e476d9068c80cc76c960a0
Arijit Swain <backend.dev@ansimap.com>
2025-08-25 18:16:24 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kampas_be.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kampas_be.kampas_be.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
