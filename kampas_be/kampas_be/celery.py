5cdf035e41634080d6a99ff3720557e581d3d898
Arijit Swain <backend.dev@ansimap.com>
2025-08-26 12:34:10 +0530

    Update celery.py

--- kampas_be/kampas_be/celery.py
+++ kampas_be/kampas_be/celery.py
@@ -2,7 +2,7 @@ import os
 from celery import Celery
 
 # Set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kampas_be.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kampas_be.kampas_be.settings')
 
 app = Celery('kampas_be')
 
