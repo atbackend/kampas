10fb33ac5ac70021c572474b64350c4c623a7e3f
Arijit Swain <backend.dev@ansimap.com>
2025-08-26 13:09:50 +0530

    Update axiosInstance.jsx

--- myapp/src/api/axiosInstance.jsx
+++ myapp/src/api/axiosInstance.jsx
@@ -4,7 +4,7 @@ import { logout, refreshTokenSuccess } from '../redux/authSlice';
 import { getStore } from "../redux/storeInjector";
 import { isTokenExpired } from '../utils/tokenUtils';
 
-const API_URL = http://127.0.0.1:8000;
+const API_URL = "http://localhost:8000";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
