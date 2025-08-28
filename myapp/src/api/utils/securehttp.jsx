c1248af626e7d1f4f560464db4bb783a6e6e6aab
Arijit Swain <backend.dev@ansimap.com>
2025-08-28 11:17:08 +0530

    Update securehttp.jsx

--- myapp/src/api/utils/securehttp.jsx
+++ myapp/src/api/utils/securehttp.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const https = axios.create({
-  baseURL: 'http://192.168.29.247:8000', 
-    // baseURL: 'http://ec2-3-108-54-217.ap-south-1.compute.amazonaws.com',
+  // baseURL: 'http://192.168.29.247:8000', 
+  baseURL: 'http://ec2-65-0-97-169.ap-south-1.compute.amazonaws.com',
   headers: {
     'Content-Type': 'application/json',
   },
